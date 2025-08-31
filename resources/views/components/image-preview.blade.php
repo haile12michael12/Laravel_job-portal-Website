5459a655889559661de65688fd3107f5321f951b
haile12michael12 <michaelhaile12@yahoo.com>
2025-08-31 10:39:21 -0700

    add the project structure

--- resources/views/components/image-preview.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['height', 'width', 'source'])
-@if ($source)
-<div>
-    <img {{ $attributes->merge(["style" => "height: {$height}px; width: {$width}px; object-fit:cover;"]) }} src="{{ $source }}" alt="">
-</div>
-@endif
--- resources/views/components/input-error.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['messages'])
-
-@if ($messages)
-    @foreach ((array) $messages as $message)
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-    @endforeach
-@endif
